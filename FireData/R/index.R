b7561fdf71b807fe06d0467e73448fbd2f2648b6
Kohze <mlw.kohze@student.ru.nl>
2017-06-22 17:41:12 +0100

    added fireData_upload classes

--- FireData/R/index.R
+++ FireData/R/index.R
@@ -7,16 +7,36 @@
 #' @return returns http request return. Includes the key.
 #' @export
 fireData_upload <- function(x, projectURL, directory = "main"){
- Response = POST(paste0(projectURL,"/","directory",".json"), body = toJSON(x, auto_unbox = TRUE))
- return(content(Response))
+ if (isS4(x)) {
+    output = fireData_classConversion(x)
+ } else {
+    output = x
+ }
+  Response = POST(paste0(projectURL,"/","directory",".json"), body = toJSON(output, auto_unbox = TRUE))
+  return(httr::content(Response))
 }
 
-#' @title FireData_Backup()
+#' @title FireData_download()
 #' @param x A data.frame or data.table
 #' @return showing shapiro.test output of the data.frame
 #' @export
 fireData_download <- function(x, projectURL, directory){
   print("works")
+}
+
+#' @title fireData_classConversion()
+#' @param x A data.frame or data.table
+#' @return showing shapiro.test output of the data.frame
+#' @export
+fireData_classConversion <- function(x){
+  #convert object to base64
+  tempPath = tempfile()
+  saveRDS(x, file = tempPath)
+  binarySet = readBin(tempPath, what = "raw", n = 50000000)
+  base64Set = jsonlite::base64_enc(binarySet)
+  #adding key by assigning to data.frame
+  pRolocList = list("base64Set" =  base64Set)
+  return(pRolocList)
 }
 
 
@@ -35,3 +55,4 @@ path_check <- function(path){
   if (path_replaced != path) warning(paste0("path changed to ", path_replaced))
   return(path)
 }
+
